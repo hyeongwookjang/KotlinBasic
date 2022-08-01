3b0fbc553690555a5c386b23662d7f1214f19021
장형욱 <janghw33@gmail.com>
2022-08-01 17:45:48 +0900

    null

---
+++ test/src/main/kotlin/com/jtk/basic/javaToKotlin/functionalProgramming/Null.kt
@@ -0,0 +1,10 @@
+package com.jtk.basic.javaToKotlin.functionalProgramming
+
+// List<Int?>        o x 리스트에 null이 들어갈 수 있지만, 리스트는 null일 수 없음
+// List<Int>?        x o 리스트에 null이 들어갈 수 없지만, 리스트는 null일 수 있음
+// List<Int?>?       o o 리스트에 null이 들어갈 수 있으며, 리스트가 null일 수 있음
+
+
+// 코틀린은 불변형이 있다. listof.
+// 코틀린은 non-nullable도 있다.
+// 코틀린에서 자바로 보낼떄 unmodified 로 보내면 수정불가하다.
